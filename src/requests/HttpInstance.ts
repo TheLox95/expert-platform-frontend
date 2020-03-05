1f176f1f3588b6d49eedad901b1b39777867cca2
leonardo.b <leonardo.b@solutionbeyond.com>
2020-03-05 08:36:21 -0400

    REMOVED unused code

--- src/requests/HttpInstance.ts
+++
@@ -1,5 +0,0 @@
-import { AxiosRequestConfig, AxiosPromise } from "axios";
-
-export default interface HttpInstance<T = {}> {
-    (config: AxiosRequestConfig & { disableGLobal?: boolean }): AxiosPromise<T>
-  }
--- src/requests/index.ts
+++ src/requests/index.ts
@@ -1,8 +1,4 @@
-import { default as HttpInstance_ } from './HttpInstance';
-
 export { default as Notification } from './Notifications';
 export { default as File } from './File';
 export { default as Offering } from './Offering';
 export { default as User } from './User';
-
-export type HttpInstance<T> = HttpInstance_<T>;
